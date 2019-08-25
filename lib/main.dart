46332a32e027f71ac1c4079a2f39bfe7d510887e
Rupesh Kumar Nidhi <send.mail.rupesh@gmail.com>
2019-08-26 01:08:53 +0545

    using in built scafold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    MaterialApp(
-      home: SafeArea(child: MyScafold()),
-      title: 'My App',
-    ),
-  );
+  runApp(MyApp());
 }
 
-class MyAppBar extends StatelessWidget {
-
-  MyAppBar({this.title});
-
-  final Widget title;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        children: <Widget>[
-          IconButton(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Example Title'),
+          leading: IconButton(
             icon: Icon(Icons.menu),
-            onPressed: null,
             tooltip: 'Navigation Menu',
+            onPressed: null,  
           ),
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: null,
-            tooltip: 'Search',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScafold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Example Title',
-              style:Theme.of(context).primaryTextTheme.title
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
             ),
+          ],
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello Wolrd'),
           ),
-          Expanded(
-            child: Center(
-              child: Text(
-                'Hello World'
-              ),
-            ),
-          ),
-        ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          tooltip: 'Add',
+          onPressed: null,
+        ),
       ),
     );
   }
